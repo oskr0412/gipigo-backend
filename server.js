ba388e75b007fec1394e187e083947ec68cb977e
Oscar Pacheco <emilio14pacheco@gmail.com>
2025-05-27 01:42:31 -0400

    Use PORT environment variable for Render compatibility

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ admin.initializeApp({
 });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
